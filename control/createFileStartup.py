54dbfa3a36ca63fd916a5f48f14f6e07483445a9
Mink Alex Vina <64893437+anh-dz@users.noreply.github.com>
2022-07-11 09:24:34 +0700

    Update createFileStartup.py

--- control/createFileStartup.py
+++ control/createFileStartup.py
@@ -16,3 +16,18 @@ def create_folder() -> bool:
         return True
     
     return False
+
+def default_data():
+    day = {"1": {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0},
+           "2": {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0},
+           "3": {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0},
+           "4": {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0},
+           "5": {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0}}
+    data = {"last time": "00:00:00", "last day": "Thursday"}
+    settings_data = {"alarmDur": 30, "startWithSys": False, "openUI": True, "sound": True}
+    with open(f"C://Users//{user}//AppData//Roaming//TimeMana//settings.json", "w") as outfile:
+        json.dump(settings_data, outfile)
+    with open(f"C://Users//{user}//AppData//Roaming//TimeMana//tim.json", "w") as outfile:
+        json.dump(data, outfile)
+    with open(f"C://Users//{user}//AppData//Roaming//TimeMana//day.json", "w") as outfile:
+         json.dump(day, outfile)
